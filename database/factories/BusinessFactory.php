6d8826632f9e5c48d29357675e0943eeed307080
Maluka Thapelo <excellentt7@gmail.com>
2023-05-26 10:13:50 +0200

    thapelo business factory update

--- database/factories/BusinessFactory.php
+++ database/factories/BusinessFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Business;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BusinessFactory extends Factory
@@ -21,37 +22,37 @@ class BusinessFactory extends Factory
      */
     public function definition()
     {
+        $user = User::factory()->create();
+
         $digits = 12;
-        $id = rand(pow(10, $digits-1), pow(10, $digits)-1);
-                $contact = 7;
-        $numb = rand(pow(10, $contact-1), pow(10, $contact)-1);
+        $id = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
+        $contact = 7;
+        $numb = rand(pow(10, $contact - 1), pow(10, $contact) - 1);
         $name = $this->faker->name();
-        $arr = explode(' ',trim($name));
+        $arr = explode(' ', trim($name));
 
         $company_name = $this->faker->company();
-        $website = explode(' ',trim(str_replace(',','',$company_name)));
+        $website = explode(' ', trim(str_replace(',', '', $company_name)));
         $company_rep = 1;
 
         return [
-            'company_rep' => $company_rep,
+            'company_rep' => $user->id,
             'business_name' => $this->faker->word(),
-            'business_number' => '0'.rand(1, 8).rand(1, 9).$numb,
-            'business_bio' => "'".$this->faker->paragraph($nbSentences = 3, $variableNbSentences = true)."'",
-            'email' => "'".$arr[0].'@'.$website[0].'.co.za'."'",
-            'provinceId' => rand(1,9),
+            'business_number' => '0' . rand(1, 8) . rand(1, 9) . $numb,
+            'business_bio' => $this->faker->text($maxNbChars = 255),
+            'email' => $arr[0] . '@' . $website[0] . '.co.za',
+            'provinceId' => rand(1, 9),
             'town' => $this->faker->city(),
-            'address' =>  "'" . $this->faker->city() . "'",
+            'address' => $this->faker->city(),
             'company_reg' => 1,
-            'website' => 'wwww.'.$website[0].'.co.za',
-            'industryId' => rand(1,10),
+            'website' => 'wwww.' . $website[0] . '.co.za',
+            'industryId' => rand(1, 8),
             'facebook' => 'facebook',
             'twitter' => 'twitter',
             'instagram' => 'instagram',
             'logo' => 'a.png',
             'marketingpic' => 'a.png',
-            'activation_status' => 1
+            'activation_status' => 1,
         ];
-
-        $company_rep +1;
     }
 }
